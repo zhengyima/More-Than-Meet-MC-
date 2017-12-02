aebffc61c3c2e7eef51867c11bf0398cf57a2266
lmerengues <317958662@qq.com>
2017-12-02 23:29:44 +0800

    detail页完成版

--- mp/detail.py
+++ mp/detail.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import json
 from django.db import connections
 
-cursor = connections['default'].cursor()
+#cursor = connections['default'].cursor()
 
 def dictfetchall(cursor):
 	desc = cursor.description
@@ -12,24 +12,26 @@ def dictfetchall(cursor):
     	for row in cursor.fetchall()
     	]
 def index(request):
-	    
+	cursor = connections['default'].cursor()	    
     #return HttpResponse("Hello world ! ")
 	sno = request.GET['sno']
-	cursor.execute("select Seller.sno,sname,sgender,sage,sheight,sweight,sxz,sschool,smajor,sgrade,stime,srange,swage,sinfo,simg,slike from Seller,Seller_display where Seller.sno = %s",(sno,))
+	cursor.execute("select Seller.sno,sname,sgender,sage,sheight,sweight,sxz,sschool,smajor,sgrade,stime,srange,swage,sinfo,simg,slike,scharm,snum from Seller where Seller.sno = %s",(sno,))
 	raw = dictfetchall(cursor)
+	cursor.close()
 
 	lcursor = connections['default'].cursor()
 	lcursor.execute("select lno,lname from Seller,Seller_label where Seller.sno = Seller_label.sno and Seller_label.sno = %s",(sno,))
 	raw[0]['labels'] = dictfetchall(lcursor)
+	lcursor.close()
 
 	icursor = connections['default'].cursor()
 	icursor.execute("select ino,iurl from Seller,Seller_image where Seller.sno = Seller_image.sno and Seller_image.sno = %s",(sno,))
-	raw[0]'images'] = dictfetchall(icursor)
+	raw[0]['images'] = dictfetchall(icursor)
 		#rawitem['arr'] = arr
 	#snoraw = fetchall(cursor)
 	#for item in snoraw:
 		#sno  = snoraw[0]
 		#cursor.execute()
-			
+	icursor.close()			
 	response = HttpResponse(json.dumps(raw),content_type="application/json")	
 	return response
--- mp/home.py
+++ mp/home.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import json
 from django.db import connections
 
-cursor = connections['default'].cursor()
+#cursor = connections['default'].cursor()
 
 def dictfetchall(cursor):
 	desc = cursor.description
@@ -12,15 +12,18 @@ def dictfetchall(cursor):
     	for row in cursor.fetchall()
     	]
 def index(request):
-	    
+
+	cursor = connections['default'].cursor()	    
     #return HttpResponse("Hello world ! ")
 	cursor.execute("select Seller.sno,sname,sgender,sage,sheight,sweight,sxz,sschool,smajor,sgrade,stime,srange,swage,sinfo,simg,slike from Seller,Seller_display where Seller.sno = Seller_display.sno and Seller_display.sflag = 1")
 	raw = dictfetchall(cursor)
+	cursor.close()
 	for rawitem in raw:
 		sno = rawitem['sno']
 		lcursor = connections['default'].cursor()
 		lcursor.execute("select lno,lname from Seller,Seller_label where Seller.sno = Seller_label.sno and Seller_label.sno = %s",(sno,))
 		rawitem['labels'] = dictfetchall(lcursor)
+		lcursor.close()
 
 		icursor = connections['default'].cursor()
 		icursor.execute("select ino,iurl from Seller,Seller_image where Seller.sno = Seller_image.sno and Seller_image.sno = %s",(sno,))
@@ -30,6 +33,7 @@ def index(request):
 	#for item in snoraw:
 		#sno  = snoraw[0]
 		#cursor.execute()
-			
+		icursor.close()
+
 	response = HttpResponse(json.dumps(raw),content_type="application/json")	
 	return response
